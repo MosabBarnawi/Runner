9f7bfedb2d98e01997efeb46f0f43c8fa0881cb0
MosabBanrnawi <just.t17@gmail.com>
2020-10-18 10:34:02 +0100

    Movement

---
+++ Assets/_Scripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public static string HORIONTAL = "Horizontal";
+    public static string VERTICAL = "Vertical";
+}
---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public bool isConstantMovement;
+
+    public float MovementSpeed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if(rb == null)
+        {
+            Debug.LogError("RigidBody not Found");
+        }
+    }
+
+    void Update()
+    {
+        if(isConstantMovement)
+        {
+            MovePlayer(1f);
+        }
+        else
+        {
+            if(Input.GetAxis(Constants.HORIONTAL) !=0 )
+            {
+                MovePlayer(Input.GetAxis(Constants.HORIONTAL));
+            }
+        }
+    }
+
+    private void MovePlayer(float direction)
+    {
+        float speed = direction * MovementSpeed * Time.deltaTime;
+        rb.velocity += new Vector3(speed , 0 , 0);
+    }
+
+    
+}
